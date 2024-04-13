dc36a659aec3da5c8b0f690559d4286007d1c7bf
JavierFerrer03 <jferrermariblanca@gmail.com>
2024-04-13 12:20:51 +0200

    Añadido contenido en el fichero inicio.php

--- app/views/inicio.php
+++ app/views/inicio.php
@@ -5,12 +5,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HealthMastery</title>
+    <link rel="stylesheet" href="web/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <header class="header">
-        
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid header__nav">
+                <div class="containerImage">
+                    <img src="web/images/logo.png" alt="" class="imageLogo">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="header__list">
+                    <ul class="list">
+                        <li class="header__items"><a href="" class="header__link"></a>Entrenamiento</li>
+                        <li class="header__items"><a href="" class="header__link"></a>Entrenamiento</li>
+                        <li class="header__items"><a href="" class="header__link"></a>Entrenamiento</li>
+                        <li class="header__items"><a href="" class="header__link"></a>Entrenamiento</li>
+                    </ul>
+                </div>
+                <div class="header__sesion">
+                    
+                </div>
+            </div>
+        </nav>
     </header>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
